951dac8b371d4f75be1d76dd07342f1b54a0dd87
Madeline Morton <MortonCMadeline@gmail.com>
2025-12-03 23:34:15 -0600

    Used sliding window row then col sum to get sums faster. Massive speedup for p1, modest speedup for p2.

--- day_04_printing_department/forklift_grid.py
+++ day_04_printing_department/forklift_grid.py
@@ -3,7 +3,7 @@ from parser.parser import read_lines, read_line, read_grid, read_list_grid, read
 from typing import List
 
 
-def part1(grid: List[str]):
+def part1_naive_sum(grid: List[str]):
     h = len(grid)
     w = len(grid[0])
 
@@ -24,18 +24,60 @@ def part1(grid: List[str]):
     return out
 
 
+def part1(lines: List[str]):
+    h = len(lines)
+    w = len(lines[0])
+
+    boxSums = boxSum(lines)
+
+    out = 0
+    for r in range(h):
+        for c in range(w):
+            if lines[r][c] == '@' and boxSums[r][c] - 1 < 4:
+                out += 1
+    return out
+
+
+def boxSum(lines: List[str]):
+    grid = [[int(c == '@') for c in line] for line in lines]
+    h = len(grid)
+    w = len(grid[0])
+    # Do 3 wide sliding window row sum, followed by 3 wide sliding window col sum.
+
+    #sums = [[int(c == '@') for c in line] for line in grid]
+    sums = [[0] * w for _ in range(h)]
+    for r in range(h):
+        curSum = 0
+        curSum += grid[r][0]
+        curSum += grid[r][1]
+        sums[r][0] = curSum
+        for c in range(1, w-1):
+            curSum += grid[r][c+1]
+            sums[r][c] = curSum
+            curSum -= grid[r][c-1]
+        sums[r][w-1] = curSum
+
+    # re-use for efficiency.
+    boxSums = grid
+    for c in range(w):
+        curSum = 0
+        curSum += sums[0][c]
+        curSum += sums[1][c]
+        boxSums[0][c] = curSum
+        for r in range(1, h-1):
+            curSum += sums[r+1][c]
+            boxSums[r][c] = curSum
+            curSum -= sums[r-1][c]
+        boxSums[h-1][c] = curSum
+    return boxSums
+
+
 def part2(grid: List[List[str]]):
     h = len(grid)
     w = len(grid[0])
 
-    sums = [[0] * w for _ in range(h)]
-    for r in range(h):
-        for c in range(w):
-            if grid[r][c] == '.': continue
-            for rr in (r-1, r, r+1):
-                for cc in (c-1, c, c+1):
-                    if 0 <= rr < h and 0 <= cc < w:
-                        sums[rr][cc] += 1
+    sums = boxSum(grid)
+
     out = 0
     remove = []
     removed = set()
@@ -63,8 +105,10 @@ def part2(grid: List[List[str]]):
 
 
 if __name__ == '__main__':
-    get_results("P1 Example", part1, read_lines, "example.txt", expected=13)
-    get_results("P1", part1, read_lines, "input.txt", expected=1533)
+    get_results("P1 Example", part1_naive_sum, read_lines, "example.txt", expected=13)
+    get_results("P1", part1_naive_sum, read_lines, "input.txt", expected=1533)
+    get_results("P1 box sum Example", part1, read_lines, "example.txt", expected=13)
+    get_results("P1 box sum", part1, read_lines, "input.txt", expected=1533)
 
     get_results("P2 Example", part2, read_list_grid, "example.txt", expected=43)
     get_results("P2", part2, read_list_grid, "input.txt", expected=9206)
