470c2926e0110c39ca210941deba7d48a20abdab
Madeline Morton <MortonCMadeline@gmail.com>
2025-12-03 23:13:35 -0600

    Clean up day 4.

--- day_04_printing_department/forklift_grid.py
+++ day_04_printing_department/forklift_grid.py
@@ -3,13 +3,30 @@ from parser.parser import read_lines, read_line, read_grid, read_list_grid, read
 from typing import List
 
 
-# sum of all 8 connected are < 4
+def part1(grid: List[str]):
+    h = len(grid)
+    w = len(grid[0])
 
-def part1(lines):
-    grid = lines
-    grid = [list(line) for line in lines]
-    h = len(lines)
-    w = len(lines[0])
+    sums = [[0] * w for _ in range(h)]
+    for r in range(h):
+        for c in range(w):
+            if grid[r][c] == '.': continue
+            for rr in (r-1, r, r+1):
+                for cc in (c-1, c, c+1):
+                    if 0 <= rr < h and 0 <= cc < w:
+                        sums[rr][cc] += 1
+    out = 0
+    for r in range(h):
+        for c in range(w):
+            if grid[r][c] == '@' and sums[r][c] - 1 < 4:
+                out += 1
+
+    return out
+
+
+def part2(grid: List[List[str]]):
+    h = len(grid)
+    w = len(grid[0])
 
     sums = [[0] * w for _ in range(h)]
     for r in range(h):
@@ -42,18 +59,12 @@ def part1(lines):
                             removed.add((rr, cc))
                             remove.append((rr, cc))
 
-
     return out
-
-
-def part2(lines):
-    return part1(lines)
-    pass
 
 
 if __name__ == '__main__':
     get_results("P1 Example", part1, read_lines, "example.txt", expected=13)
-    get_results("P1", part1, read_lines, "input.txt")
+    get_results("P1", part1, read_lines, "input.txt", expected=1533)
 
-    get_results("P2 Example", part2, read_lines, "example.txt", expected=43)
-    get_results("P2", part2, read_lines, "input.txt")
+    get_results("P2 Example", part2, read_list_grid, "example.txt", expected=43)
+    get_results("P2", part2, read_list_grid, "input.txt", expected=9206)
--- run_days.py
+++ run_days.py
@@ -8,6 +8,7 @@ import template.template
 import day_01_secret_entrance.secret_entrance as    day_01
 import day_02_gift_shop.gift_shop as                day_02
 import day_03_lobby.joltages as                     day_03
+import day_04_printing_department.forklift_grid as  day_04
 
 day_information = {
 # # DAY: (p1, p2, input_fn, [input_fn2], "dir")
@@ -15,6 +16,7 @@ day_information = {
     1: (day_01.part1, day_01.part2, read_lines, "day_01_secret_entrance"),
     2: (day_02.part1, day_02.part2, read_line, "day_02_gift_shop"),
     3: (day_03.part1, day_03.part2, read_lines, "day_03_lobby"),
+    4: (day_04.part1, day_04.part2, read_lines, read_list_grid, "day_04_printing_department"),
 }
 
 
