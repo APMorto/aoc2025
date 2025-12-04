9e59ab2ccbbb61fad1bf4804db64a73d1e0992e1
Madeline Morton <MortonCMadeline@gmail.com>
2025-12-03 23:08:26 -0600

    day 4 p2, but I accidentally put it in p1 again, oops!

--- day_04/day4.py
+++ day_04/day4.py
@@ -7,6 +7,7 @@ from typing import List
 
 def part1(lines):
     grid = lines
+    grid = [list(line) for line in lines]
     h = len(lines)
     w = len(lines[0])
 
@@ -19,15 +20,34 @@ def part1(lines):
                     if 0 <= rr < h and 0 <= cc < w:
                         sums[rr][cc] += 1
     out = 0
-
+    remove = []
+    removed = set()
     for r in range(h):
         for c in range(w):
             if grid[r][c] == '@' and sums[r][c] - 1 < 4:
+                grid[r][c] = '.'
+                removed.add((r, c))
+                remove.append((r, c))
                 out += 1
+    while remove:
+        r, c = remove.pop()
+        for rr in (r-1, r, r+1):
+            for cc in (c-1, c, c+1):
+                if 0 <= rr < h and 0 <= cc < w:
+                    if grid[rr][cc] == '@':
+                        sums[rr][cc] -= 1
+                        if (rr, cc) not in removed and sums[rr][cc] - 1 < 4:
+                            grid[rr][cc] = '.'
+                            out += 1
+                            removed.add((rr, cc))
+                            remove.append((rr, cc))
+
+
     return out
 
 
 def part2(lines):
+    return part1(lines)
     pass
 
 
@@ -35,5 +55,5 @@ if __name__ == '__main__':
     get_results("P1 Example", part1, read_lines, "example.txt", expected=13)
     get_results("P1", part1, read_lines, "input.txt")
 
-    get_results("P2 Example", part2, read_lines, "example.txt", expected=None)
+    get_results("P2 Example", part2, read_lines, "example.txt", expected=43)
     get_results("P2", part2, read_lines, "input.txt")
