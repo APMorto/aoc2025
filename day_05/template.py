a7e4a64608c43e5f21ee651d14abbbd26e180105
Madeline Morton <MortonCMadeline@gmail.com>
2025-12-04 23:07:23 -0600

    day 5 p2

--- day_05/template.py
+++ day_05/template.py
@@ -1,6 +1,7 @@
 from util.timer import get_results
 from parser.parser import read_lines, read_line, read_grid, read_list_grid, read_line_blocks
 from typing import List
+import math
 
 
 def part1(lines_groups):
@@ -22,14 +23,32 @@ def part1(lines_groups):
         if valid: out += 1
     return out
 
+def merge(intervals: List[List[int]]) -> List[List[int]]:
+    intervals.sort()  # sort by interval start (sort implicitly does this, and then breaks ties by second element)
+    nonOverlapping = [[-math.inf, -math.inf]]
+    for start, end in intervals:
+        if start <= nonOverlapping[-1][1]:
+            nonOverlapping[-1][1] = max(nonOverlapping[-1][1], end)
+        else:
+            nonOverlapping.append([start, end])
+    return nonOverlapping[1:]
 
-def part2(lines):
-    pass
+def part2(lines_groups):
+    ranges = []
+    for line in lines_groups[0]:
+        l, r = line.split("-")
+        l, r = int(l), int(r)
+        ranges.append((l, r))
+    merged = merge(ranges)
+    out = 0
+    for l, r in merged:
+        out += r - l + 1
+    return out
 
 
 if __name__ == '__main__':
     get_results("P1 Example", part1, read_line_blocks, "example.txt", expected=3)
     get_results("P1", part1, read_line_blocks, "input.txt")
 
-    get_results("P2 Example", part2, read_lines, "example.txt")
-    get_results("P2", part2, read_lines, "input.txt")
+    get_results("P2 Example", part2, read_line_blocks, "example.txt", expected=14)
+    get_results("P2", part2, read_line_blocks, "input.txt")
