a492677672caf19bc3dc4aa0c510e3c8246de48e
Madeline Morton <MortonCMadeline@gmail.com>
2025-12-05 22:54:30 -0600

    Minor template change.

--- template/template.py
+++ template/template.py
@@ -3,19 +3,17 @@ from parser.parser import read_lines, read_line, read_grid, read_list_grid, read
 from typing import List
 
 
-def part1(_):
-    print("_", _)
+def part1(lines):
     pass
 
 
-def part2(_):
-    print("_", _)
+def part2(lines):
     pass
 
 
 if __name__ == '__main__':
-    get_results("P1 Example", part1, read_lines, "example.txt")
+    get_results("P1 Example", part1, read_lines, "example.txt", expected=None)
     get_results("P1", part1, read_lines, "input.txt")
 
-    get_results("P2 Example", part2, read_lines, "example.txt")
+    get_results("P2 Example", part2, read_lines, "example.txt", expected=None)
     get_results("P2", part2, read_lines, "input.txt")
