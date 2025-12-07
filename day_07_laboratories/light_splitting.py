0e84594e837c1ab0fccc5f744c2011c6a2d8494f
Madeline Morton <MortonCMadeline@gmail.com>
2025-12-06 23:18:28 -0600

    Cleaned up day 7. Template is now back to how it should be, day 7 is in day 7

--- day_07_laboratories/light_splitting.py
+++ day_07_laboratories/light_splitting.py
@@ -3,14 +3,20 @@ from parser.parser import read_lines, read_line, read_grid, read_list_grid, read
 from typing import List
 
 
-def part1(grid):
-    start = None
+def start_pos(grid):
     w = len(grid[0])
     h = len(grid)
     for r in range(h):
         for c in range(w):
             if grid[r][c] == 'S':
                 start = (r, c)
+                return start
+    return None
+
+def part1(grid):
+    w = len(grid[0])
+    h = len(grid)
+    start = start_pos(grid)
     assert start is not None
     sr, sc = start
 
@@ -34,20 +40,12 @@ def part1(grid):
         curSet = split(rowPos, r)
         rowPos = curSet
     return splits
-    return len(rowPos)
-
-
-
 
 
 def part2(grid):
-    start = None
     w = len(grid[0])
     h = len(grid)
-    for r in range(h):
-        for c in range(w):
-            if grid[r][c] == 'S':
-                start = (r, c)
+    start = start_pos(grid)
     assert start is not None
     sr, sc = start
 
@@ -64,10 +62,9 @@ def part2(grid):
     return sum(dp[-1])
 
 
-
 if __name__ == '__main__':
     get_results("P1 Example", part1, read_lines, "example.txt", expected=21)
-    get_results("P1", part1, read_lines, "input.txt")
+    get_results("P1", part1, read_lines, "input.txt", expected=1570)
 
     get_results("P2 Example", part2, read_lines, "example.txt", expected=40)
-    get_results("P2", part2, read_lines, "input.txt")
+    get_results("P2", part2, read_lines, "input.txt", expected=15118009521693)
--- run_days.py
+++ run_days.py
@@ -4,13 +4,14 @@ import sys
 from util.timer import get_results
 from parser.parser import read_grid, read_list_grid, read_line, read_line_blocks, read_lines, read_lines_literal
 
-import template.day07
+import template.template
 import day_01_secret_entrance.secret_entrance as    day_01
 import day_02_gift_shop.gift_shop as                day_02
 import day_03_lobby.joltages as                     day_03
 import day_04_printing_department.forklift_grid as  day_04
 import day_05_cafeteria.ranges as                   day_05
 import day_06_trash_compactor.transpose_math as     day_06
+import day_07_laboratories.light_splitting as       day_07
 
 day_information = {
 # # DAY: (p1, p2, input_fn, [input_fn2], "dir")
@@ -21,6 +22,7 @@ day_information = {
     4: (day_04.part1, day_04.part2, read_lines, read_list_grid, "day_04_printing_department"),
     5: (day_05.part1, day_05.part2, read_line_blocks, "day_05_cafeteria"),
     6: (day_06.part1, day_06.part2, read_lines, read_lines_literal, "day_06_trash_compactor"),
+    7: (day_07.part1, day_07.part2, read_lines, "day_07_laboratories"),
 }
 
 
